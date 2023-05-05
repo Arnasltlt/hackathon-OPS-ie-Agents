06a62f4021e0f82b5a7a58a72d54ea59a44f7f2c
Arnasltlt <arnoldas.kemeklis@gmail.com>
2023-05-04 22:16:09 -0500

    able to pick tools and execute

--- main.py
+++ main.py
@@ -7,13 +7,41 @@ from tools import *
 
 openai.api_key = os.environ["openai_key"]
 
+conversation_history = [
+    {"role": "assistant",
+     "content": " The order is late - what would you like to do? "}
+]
 
-conversation = "I think we should meet with the client. The name is Harry potter and monday april 2022 at 5pm."
+found_definitions = False
 
-tool = pick_tool(conversation)
+while not found_definitions:
+    user_input = input("User: ")
+    conversation_history.append({"role": "user", "content": user_input})
+
+    assistant_response = retrieve_tool_and_params_definition(conversation_history)
+    conversation_history.append({"role": "assistant", "content": assistant_response})
+
+    print("Assistant:", assistant_response)
+
+    if assistant_response.startswith("Definitions found:"):
+        found_definitions = True
+
+# Extract tool_name and parameters_definition from assistant_response
+definitions = assistant_response[len("Definitions found: "):].split(", ", 1)
+tool_name = definitions[0].strip()
+parameters_definition = definitions[1].strip()
+
+print("Tool Name:", tool_name)
+print("Parameters Definition:", parameters_definition)
+
+
+print(conversation_history)
+
+
+tool = pick_tool(conversation_history)
 
 if tool:
-    tool_params = extract_tool_parameters(tool.__name__, conversation)
+    tool_params = extract_tool_parameters(tool.__name__, conversation_history)
     print("Extracted parameters:", tool_params)
 
     # Call the tool function with the extracted parameters
--- tools.py
+++ tools.py
@@ -22,15 +22,42 @@ def process_refund(order_id):
 def update_inventory(product_id, new_quantity):
     return f"Inventory updated for product {product_id}. New quantity: {new_quantity}."
 
+def retrieve_tool_and_params_definition(conversation):
+    interpretation = openai.ChatCompletion.create(
+        model="gpt-4",
+        messages=[
+            {"role": "system", "content": "You are an assistant that helps retrieve tool and parameter definitions from the conversation. "
+                                          "You have the following tools available: 'generate_price_quote', 'schedule_meeting', 'process_refund', 'update_inventory'. "
+                                          "The required parameters for each tool are: "
+                                          "'generate_price_quote' needs 'item_id and quantity', "
+                                          "'schedule_meeting' needs 'client_name, date, and time', "
+                                          "'process_refund' needs 'order_id', "
+                                          "and 'update_inventory' needs 'product_id and new_quantity'. "
+                                          "When you identify both the tool and parameter definitions, reply with 'Definitions found: TOOL_NAME, PARAMETERS_DEFINITION'. "
+                                          "Otherwise, continue the conversation to gather more information."
+                                          " The agent starts the conversation with a notification to the user. Use that to guide the user to make a decission"},
+            *conversation,
+        ],
+        temperature=0.3,
+        max_tokens=100,
+    )
+
+    response = interpretation.choices[0].message.content.strip()
+    return response
+
+
+
 # Tool picking function using OpenAI
 def pick_tool(conversation):
     interpretation = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
             {"role": "system", "content": "You are a tool picker that identifies the appropriate action based on the user's conversation. "
-                                          "You can choose from the following tools: 'generate_price_quote', 'schedule_meeting', 'process_refund', 'update_inventory'. "
-                                          "eturn the exact name of the tool. Mention only the word - no further explanations needed"},
-            {"role": "user", "content": conversation},
+                                          "As your final answer you only can choose from the following words: 'generate_price_quote', 'schedule_meeting', 'process_refund', 'update_inventory'. "
+                                          "return the exact name of the tool. Mention only the word - no further explanations needed."},
+            *conversation,
+            {"role": "system",
+             "content": "Here's the tool you've chosen:"},
         ],
         temperature=0.3,
         max_tokens=50,
@@ -63,13 +90,22 @@ def extract_tool_parameters(tool_name, conversation):
         messages=[
             {"role": "system", "content": f"You are an assistant that extracts relevant parameters from the conversation for the '{tool_name}' tool. "
                                           f"The required parameters for this tool are: {params_info}. "
-                                          "Please extract the required parameters in a JSON format.No further explanation needed"},
-            {"role": "user", "content": conversation},
+                                          """Please extract the required parameters in a JSON format just like this: {"item_id": '213', "quantity": '44'} .No further explanation needed"""""},
+
+            *conversation,
+            {"role": "system",
+             "content": "The final answer:"},
         ],
         temperature=0.3,
         max_tokens=100,
     )
+    print(interpretation.choices[0].message.content)
+    extracted_params_json = interpretation.choices[0].message.content
+    try:
+        extracted_params = json.loads(extracted_params_json)
+    except json.JSONDecodeError:
+        print("Error: Invalid JSON format.")
+        extracted_params = None
 
-    extracted_params_json = interpretation.choices[0].message.content.strip()
-    return json.loads(extracted_params_json)
+    return extracted_params
 
