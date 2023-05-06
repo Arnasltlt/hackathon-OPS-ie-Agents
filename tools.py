4deca765f9c4dc1673aaa2f0e22eb6a79d589dd2
Arnasltlt <arnoldas.kemeklis@gmail.com>
2023-05-06 13:54:46 -0500

    added tools

--- tools.py
+++ tools.py
@@ -1,7 +1,39 @@
 import openai
 import json
+import requests
 import os
 
+freshdesk_auth = os.environ['freshdesk_auth']
+zap_auth = os.environ['zap_api']
+
+def plain_gpt(instructions,input):
+    interpretation = openai.ChatCompletion.create(
+        model="gpt-4",
+        messages=[
+            {"role": "system", "content": f"{instructions}"},
+            {"role": "user", "content": f"{input}"},
+        ],
+        temperature=0.3,
+        max_tokens=400,
+    )
+
+    return interpretation.choices[0].message.content
+
+def get_conversations(ticket_id,summary_instructions):
+    url = f"https://3dhubs.freshdesk.com/api/v2/tickets/{ticket_id}/conversations"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": freshdesk_auth
+    }
+
+    response = requests.get(url, headers=headers)
+
+
+    if response.status_code == 200:
+        text = response.json()[0]['body_text']
+        print(text)
+        summary = plain_gpt(f'Summarise this text for me. ---{summary_instructions} ---',text)
+        return summary
 
 # Tool function example: Generate a price quote
 def generate_price_quote(item_id, quantity):
@@ -21,7 +53,13 @@ def process_refund(order_id):
 def update_inventory(product_id, new_quantity):
     return f"Inventory updated for product {product_id}. New quantity: {new_quantity}."
 
+<<<<<<< Updated upstream
 def retrieve_tool_and_params_definition(conversation):
+=======
+
+
+def retrieve_tool_and_params_definition(conversation_history):
+>>>>>>> Stashed changes
     interpretation = openai.ChatCompletion.create(
         model="gpt-4",
         messages=[
@@ -53,7 +91,7 @@ def pick_tool(conversation):
         model="gpt-3.5-turbo",
         messages=[
             {"role": "system", "content": "You are a tool picker that identifies the appropriate action based on the user's conversation. "
-                                          "As your final answer you only can choose from the following words: 'generate_price_quote', 'schedule_meeting', 'process_refund', 'update_inventory'. "
+                                          "As your final answer you only can choose from the following words:'get_conversations' 'generate_price_quote', 'schedule_meeting', 'process_refund', 'send_email'. "
                                           "return the exact name of the tool. Mention only the word - no further explanations needed."},
             *conversation,
             {"role": "system",
@@ -70,7 +108,8 @@ def pick_tool(conversation):
         "generate_price_quote": generate_price_quote,
         "schedule_meeting": schedule_meeting,
         "process_refund": process_refund,
-        "update_inventory": update_inventory,
+        "send_email": send_email,
+        "get_conversations" : get_conversations,
     }
     print("Tool selected: ",tool_name)
     return tools.get(tool_name, None)
@@ -81,7 +120,8 @@ def extract_tool_parameters(tool_name, conversation):
         "generate_price_quote": "item_id and quantity",
         "schedule_meeting": "client_name, date, and time",
         "process_refund": "order_id",
-        "update_inventory": "product_id and new_quantity"
+        "send_email": "email content",
+        "get_conversations": "ticket_id and summary instructions"
     }
     params_info = params_definition.get(tool_name, "")
 
@@ -109,3 +149,19 @@ def extract_tool_parameters(tool_name, conversation):
 
     return extracted_params
 
+
+def send_email(instructions):
+    url = "https://nla.zapier.com/api/v1/exposed/01GZS5GHDWRKMCPCN9ZA6DJVV0/execute/"
+
+    headers = {
+        "Content-Type": "application/json",
+        "x-api-key": zap_auth,
+    }
+
+    data = {
+        "instructions": f"send an email using the following instructions: {instructions} ",
+    }
+
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+
+    print(response.text)
